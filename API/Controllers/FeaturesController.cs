79da76a3b740258fde2011acd4a67ef860a01685
Joseph.Ballard <joseph.ballard@tiga.us>
2020-08-19 21:52:10 -0500

    initial commit

---
+++ API/Controllers/FeaturesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public FeaturesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Feature>>> GetFeatures()
+        {
+            var features = await _context.Features.ToListAsync();
+
+            return Ok(features);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Feature>> GetFeature(int id)
+        {
+            return  await _context.Features.FindAsync(id);
+        }
+
+    }
+}
---
+++ Core/Entities/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Discovery { get; set; }
+        public decimal Design { get; set; }
+        public decimal Test { get; set; }
+
+    }
+}
